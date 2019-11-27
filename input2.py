7ea36a1cf88ffe1053661d7a38be96809b5076c2
kevinrizki1019 <kevinrizkimohammad@gmail.com>
2019-11-27 08:27:13 +0700

    Add input2

---
+++ input2.py
@@ -0,0 +1,8 @@
+from kamarkost import tidurmalam
+import kebahagiaan
+import rasasenang
+import kedamaiaan
+
+KEBAHAGIAAN_INPUT_NAME = "input.jpg"
+KESENANGANAN_OUTPUT_NAME = "result.txt"
+ISI_HATI = []
