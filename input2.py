88552acf01aae6fd39d9c79b77ff85854e289131
kevinrizki1019 <kevinrizkimohammad@gmail.com>
2019-11-27 09:05:40 +0700

    Add rule for period word

--- input2.py
+++ input2.py
@@ -6,3 +6,11 @@ import kedamaiaan
 KEBAHAGIAAN_INPUT_NAME = "input.jpg"
 KESENANGANAN_OUTPUT_NAME = "result.txt"
 ISI_HATI = []
+for x in range(32,127):
+    ISI_HATI.append(x)
+
+def extract_kebahagiaan_from_isi_hati():
+    PIPE_ASCII = 124
+    
+    time_start = time.perf_counter()
+    text = pyterseract.image_to_string(Image.open(IMAGE_INPUT_NAME))
